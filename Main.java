a8c2f4e3e6db0f8420bdada60395c54262b332c3
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-16 18:16:36 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package ProjetoTestes;
+
+public class Main {
+	public static void main(String[] args) {
+		Midia img = new Video("/home/images/MotivosPraChorar/morena.png", "720x480");
+		System.out.println(img.toString());
+	}
+}
