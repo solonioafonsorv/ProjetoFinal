c1ef10f175ed2ebdea815e82c9a89e79fb09a95d
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-23 11:09:47 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package ProjetoTestes;
-
-public class Main {
-	public static void main(String[] args) {
-		Midia img = new Video("/home/images/MotivosPraChorar/morena.png", "720x480");
-		System.out.println(img.toString());
-	}
-}
